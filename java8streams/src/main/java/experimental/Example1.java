16e2627dee6bb32257c20bb7a0a1b90fc5ee695e
Jurij Albert <juri.albert@gmail.com>
2019-10-05 15:33:14 +0200

    commited changes

--- java8streams/src/main/java/experimental/Example1.java
+++ java8streams/src/main/java/experimental/Example1.java
@@ -3,8 +3,10 @@ package experimental;
 /**
  * Hello world!
  */
-public class App {
+public class Example1 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        //some changes - unsure it will work
+
     }
 }
